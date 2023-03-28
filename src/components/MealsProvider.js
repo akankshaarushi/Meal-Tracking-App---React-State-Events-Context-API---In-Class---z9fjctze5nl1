ebd54421316b479702cf43021bdd7fef52b5d21a
Akanksha Kumari <115232645+akankshaarushi@users.noreply.github.com>
2023-03-28 16:56:00 +0530

    Update MealsProvider.js

--- src/components/MealsProvider.js
+++ src/components/MealsProvider.js
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 
 const todaysMeals = [
-    { id: 1, name: 'Baked Beans' },
-    { id: 2, name: 'Baked Sweet Potatoes' },
-    { id: 3, name: 'Baked Potatoes' },
-]
+  { id: 1, name: "Baked Beans", ticked: false },
+  { id: 2, name: "Baked Sweet Potatoes", ticked: false },
+  { id: 3, name: "Baked Potatoes", ticked: false },
+];
 
-const MealsProvider = () => {
-    const [meals, setMeals] = useState(todaysMeals);
+export const MealsContext = createContext();
 
-    const tickMeal = () => {
+const MealsProvider = ({ children }) => {
+  const [meals, setMeals] = useState(todaysMeals);
 
-    }
+  const tickMeal = (id) => {
+    const updatedMeals = meals.map((meal) => {
+      if (meal.id === id) {
+        return { ...meal, ticked: !meal.ticked };
+      } else {
+        return meal;
+      }
+    });
+    setMeals(updatedMeals);
+  };
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <MealsContext.Provider value={{ meals, tickMeal }}>
+      {children}
+    </MealsContext.Provider>
+  );
 };
 
 export default MealsProvider;
